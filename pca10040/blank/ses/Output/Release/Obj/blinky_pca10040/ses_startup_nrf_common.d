8e67c9043c0d705e3d30834e09a0e2593f3b5865
Jared Botte <jaredbotte@gmail.com>
2021-09-16 16:54:53 -0400

    Fixed .gitignore

--- pca10040/blank/ses/Output/Release/Obj/blinky_pca10040/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Release/Obj/blinky_pca10040/ses_startup_nrf_common.o: \
- /Users/jared/Documents/senior-design/Device/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/ses_startup_nrf_common.s
--- pca10040/blank/ses/Output/Release/Obj/blinky_pca10040/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Release/Obj/blinky_pca10040/thumb_crt0.o: \
- /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.60a/source/thumb_crt0.s
